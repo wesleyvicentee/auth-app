f11db55279523ac753f2c347596a1c2430fd8352
Wesley Vicente <wesley@maismei.com.br>
2026-02-08 16:35:53 -0300

    first configs

---
+++ src/routes/AuthStack.tsx
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+    );
+}
